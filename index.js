fa467b1cec663ad1ca3fb813ce36233939796076
Bangles24 <48773779+Bangles24@users.noreply.github.com>
2019-03-20 20:47:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  
 });
 
 client.on('message', msg => {
-  if (msg.content === ')setup') {
-    msg.reply('Is this the channel you want to set up Endless Horse in? (yes/no)');
-      if (msg.content === 'yes') {
-        msg.reply ('Setting up in ${msg.channel}');
-      } else if (msg.content === 'no') {
-        msg.reply ('Please say this command in the channel you want me to be set up in');
-      } else {
-        msg.reply ('Error');
-    }
+  if (msg.content === ':horse') {
+    let horse = msg.guild.channels.find(`name`."horse");
+    horse.send('Test')
   }});
 
 client.login(process.env.BOT_TOKEN);
