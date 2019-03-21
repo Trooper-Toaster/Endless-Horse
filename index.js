db5b015000617bda3646ef938936f9312368d4f6
Bangles24 <48773779+Bangles24@users.noreply.github.com>
2019-03-20 20:23:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === ')setup') {
+    msg.reply('Is this the channel you want to set up Endless Horse in? (yes/no)');
+      if (msg.content === 'yes') {
+        msg.reply ('Setting up in ${msg.channel}');
+      } else if (msg.content === 'no') {
+        msg.reply ('Please say this command in the channel you want me to be set up in');
+      } else {
+        msg.reply ('Error');
   }
 });
 
