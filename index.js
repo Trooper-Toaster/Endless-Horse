401a173801022c794c3818cd9d2855d24775de42
Bangles24 <48773779+Bangles24@users.noreply.github.com>
2019-03-20 20:39:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-})
+});
 
 client.on('message', msg => {
   if (msg.content === ')setup') {
@@ -14,7 +14,7 @@ client.on('message', msg => {
         msg.reply ('Please say this command in the channel you want me to be set up in');
       } else {
         msg.reply ('Error');
-  }
-};
+    }
+  }});
 
 client.login(process.env.BOT_TOKEN);
